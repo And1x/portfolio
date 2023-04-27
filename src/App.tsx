78fae3a29e3fb01b35bbc44c73a2f459b44188ef
And1 <92379947+And1x@users.noreply.github.com>
2023-04-27 21:15:20 +0200

    layout, navbar with darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
+import Skills from "./components/Skills";
+
 function App() {
+  const [dark, setDark] = useState(false);
   return (
-    <>
-      <div className="bg-gray-500">1</div>
-      <div>2</div>
-    </>
+    <div className={`flex flex-col ${dark ? "dark" : ""}`}>
+      <Navbar toggleDark={() => setDark(!dark)} />
+      <About />
+      <Portfolio />
+      <Skills />
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,44 @@
+import SunIcon from "/src/assets/sun.svg";
+import MoonIcon from "/src/assets/moon.svg";
+
+export default function Navbar({ toggleDark }: { toggleDark: () => void }) {
+  return (
+    <header className="bg-gray-100 dark:bg-gray-900 dark:text-gray-50 shadow-lg px-10 py-4 ">
+      <div className="flex justify-between items-center text-xl">
+        <nav>
+          <a
+            href="#About"
+            className="mr-4 hover:underline hover:decoration-orange-500"
+          >
+            Andi
+          </a>
+
+          <a
+            href="#Projects"
+            className="mr-4 hover:underline hover:decoration-orange-500"
+          >
+            Projects
+          </a>
+
+          <a
+            href="#Skills"
+            className="hover:underline hover:decoration-orange-500"
+          >
+            Skills
+          </a>
+        </nav>
+        <button
+          className="relative rounded bg-gray-200 hover:outline hover:outline-orange-500 dark:bg-gray-700"
+          onClick={toggleDark}
+        >
+          <div className="absolute right-0 dark:invisible max-h-full">
+            <img src={MoonIcon} alt="moon" />
+          </div>
+          <div className="absoulute right-0 invisible dark:visible max-h-full">
+            <img src={SunIcon} alt="sun" />
+          </div>
+        </button>
+      </div>
+    </header>
+  );
+}
