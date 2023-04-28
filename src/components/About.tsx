cce41595c308d340779e5d6d58fad2b3c9c2eaab
And1 <92379947+And1x@users.noreply.github.com>
2023-04-28 10:15:18 +0200

    basic about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,3 +1,12 @@
 export default function About() {
-  return <div>About</div>;
+  return (
+    <section id="about" className="bg-gray-50 dark:bg-gray-800  pl-20 py-16">
+      <h1 className="text-5xl text-slate-900 dark:text-white">
+        Hi, I am Andi.
+      </h1>
+      <h2 className="text-3xl text-gray-700 dark:text-gray-300">
+        A Fullstack Developer with a passion for Bitcoin and Nostr.
+      </h2>
+    </section>
+  );
 }
