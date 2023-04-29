b88432ca68d5ce7140d14361f116ac17a99b1fc0
And1 <92379947+And1x@users.noreply.github.com>
2023-04-29 17:56:00 +0200

    skill section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ export default function About() {
       id="about"
       className="bg-gray-50 dark:bg-gray-800 sm:pl-20 pl-10 py-16"
     >
-      <h1 className="text-5xl text-slate-900 dark:text-white">
+      <h1 className="text-6xl text-slate-900 dark:text-white">
         Hi, I am Andi.
       </h1>
       <h2 className="text-3xl text-gray-700 dark:text-gray-300">
