0d17f6b07a2ec7c2ea2ffb0bb6d19d3989486705
And1 <92379947+And1x@users.noreply.github.com>
2023-05-02 19:34:02 +0200

    improved about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,13 +2,15 @@ export default function About() {
   return (
     <section
       id="about"
-      className="bg-gray-50 dark:bg-gray-800 sm:pl-20 pl-10 sm:py-16 py-8"
+      className="bg-gray-50 dark:bg-gray-800 sm:pl-32 pl-10 pr-6 sm:py-16 py-8"
     >
       <h1 className="text-4xl sm:text-6xl text-slate-900 dark:text-white">
         Hi, I am Andi.
       </h1>
-      <h2 className="text-xl sm:text-3xl text-gray-700 dark:text-gray-300">
-        A Fullstack Developer with a passion for Bitcoin and Nostr.
+      <h2 className="text-xl sm:text-3xl sm:max-w-3xl max-w-lg text-gray-700 dark:text-gray-300">
+        I enjoy building websites and web applications using the latest
+        technologies, especially those related to Bitcoin and Nostr. Let me show
+        you some of my projects!
       </h2>
     </section>
   );
